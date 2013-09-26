99b7d9099b13e2bb3776f653da0621f1ebb40f00
android12345 <fengxue258@126.com>
2013-09-26 13:54:54 +0800

    修改主页

--- src/com/youdao/techmarket/HomeActivity.java
+++ src/com/youdao/techmarket/HomeActivity.java
@@ -49,6 +49,14 @@ public class HomeActivity extends BaseActivity  {
 //		}) ;
 		
 		
+	
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		//cordovaWebView.loadUrl("file:///android_asset/home/index.html") ;
 		String loadInfo = getIntent().getStringExtra("appendurl") ;
 		if(loadInfo!=null && !"".equals(loadInfo)){
 			super.initAndLoadUrl(cordovaWebView, "file:///android_asset/home/index.html#"+loadInfo);
@@ -60,13 +68,6 @@ public class HomeActivity extends BaseActivity  {
 		//	Toast.makeText(HomeActivity.this, "调用了",0).show() ;
 			super.initAndLoadUrl(cordovaWebView, "file:///android_asset/home/index.html");
 		}
-
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		//cordovaWebView.loadUrl("file:///android_asset/home/index.html") ;
 	}
 
 	@Override
