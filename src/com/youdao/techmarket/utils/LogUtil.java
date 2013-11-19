d0cb0fc38d308697e2b1b6fbee6ff4c6e43cba8a
jun <fengxue258@126.com>
2013-11-19 10:21:42 +0800

    添加LogUtils类

---
+++ src/com/youdao/techmarket/utils/LogUtil.java
@@ -0,0 +1,61 @@
+package com.youdao.techmarket.utils;
+
+import com.youdao.techmarket.BuildConfig;
+/**
+ * 公共的日志打印类，在测试版的时候方便调试
+ * @author fengxue
+ *
+ */
+public class LogUtil {
+	public static boolean isDebug = BuildConfig.DEBUG; //默认debug下是true,如果打成签名包就自动变成false了
+
+	public static void v(String tag, String msg) {
+		if (isDebug)
+			android.util.Log.v(tag, msg);
+	}
+
+	public static void v(String tag, String msg, Throwable t) {
+		if (isDebug)
+			android.util.Log.v(tag, msg, t);
+	}
+
+	public static void d(String tag, String msg) {
+		if (isDebug)
+			android.util.Log.d(tag, msg);
+	}
+
+	public static void d(String tag, String msg, Throwable t) {
+		if (isDebug)
+			android.util.Log.d(tag, msg, t);
+	}
+
+	public static void i(String tag, String msg) {
+		if (isDebug)
+			android.util.Log.i(tag, msg);
+	}
+
+	public static void i(String tag, String msg, Throwable t) {
+		if (isDebug)
+			android.util.Log.i(tag, msg, t);
+	}
+
+	public static void w(String tag, String msg) {
+		if (isDebug)
+			android.util.Log.w(tag, msg);
+	}
+
+	public static void w(String tag, String msg, Throwable t) {
+		if (isDebug)
+			android.util.Log.w(tag, msg, t);
+	}
+
+	public static void e(String tag, String msg) {
+		if (isDebug)
+			android.util.Log.e(tag, msg);
+	}
+
+	public static void e(String tag, String msg, Throwable t) {
+		if (isDebug)
+			android.util.Log.e(tag, msg, t);
+	}
+}
